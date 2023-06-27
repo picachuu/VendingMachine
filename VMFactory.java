c882e369e64cb8f4dee5a76bb8d72b93bb772c60
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-28 06:02:52 +0800

    added create menu draft

--- VMFactory.java
+++ VMFactory.java
@@ -7,7 +7,7 @@ public class VMFactory {
         Scanner sc = new Scanner(System.in);
         boolean willLoop = false;
 
-        do {
+        do{
             System.out.println("\n----------Main Menu----------");
             System.out.println("[1] Create a Vending Machine");
             System.out.println("[2] Test a Vending Maachine");
@@ -16,13 +16,29 @@ public class VMFactory {
             int nChoice = sc.nextInt();
             switch(nChoice) {
                 case 1:
-                    //isValid = true;
                     System.out.print("\033[H\033[2J");
-                    //test purposes
-                    RegularVM vm = new RegularVM();
-                    System.out.println("Create Screen");
-                    vm.displayItems();
-                    String buff = sc.next();
+                    boolean isValid = false;
+                    
+                    do{
+                        System.out.println("\n----------Create----------");
+                        System.out.println("[1] Regular Vending Machine");
+                        System.out.println("[2] Special Vending Maachine");
+                        System.out.print("Input: ");
+                        int nCreateChoice = sc.nextInt();
+                        switch(nCreateChoice) {
+                            case 1:
+                                isValid = true;
+                                RegularVM vm = new RegularVM();
+                                vm.displayItems();
+                                String buff = sc.next();
+                                break;
+                            case 2:
+                                System.out.println("Sorry! This feature is not yet available.\n");
+                                break;
+                            default:
+                                System.out.println("Sorry, that is not option. Please re-enter input:");
+                        }
+                    }while (!isValid);
                     System.out.print("\033[H\033[2J");
                     break;
                 case 2:
