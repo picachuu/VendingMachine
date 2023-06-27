281720578abee995a02b086e3a5f1108840c6914
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-28 02:49:50 +0800

    add displayitems

--- RegularVM.java
+++ RegularVM.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class RegularVM {
+    private ArrayList<Item> Slot;
+
+    public RegularVM(){
+        Slot = new ArrayList<>();
+    }
+
+    public void displayItems(){
+        System.out.printf("%23s", "Items");
+        System.out.println("-------------------------------------");
+        System.out.printf("\n%10s%22s", "Name", "Price");
+        System.out.println("-------------------------------------");
+        for (int i = 1; i < Slot.size(); i++)
+        {
+            System.out.printf("%d.) %-17s%10f", i, Item.getName(), Item.getPrice());
+
+        }
+
+
+    }
+
+
     
 }
