fd1ac7eb9f5fe1cf47cc6d40e1628b166cf03e0d
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-28 02:09:50 +0800

    improve main menu

--- VMFactory.java
+++ VMFactory.java
@@ -5,30 +5,34 @@ public class VMFactory {
 
         //Main Menu
         Scanner sc = new Scanner(System.in);
-        System.out.println("[1] Create a Vending Machine");
-        System.out.println("[2] Test a Vending Maachine");
-        System.out.println("[3] Exit");
+        boolean willLoop = false;
 
-        boolean isValid = false;
-
-        while (!isValid) {
+        do {
+            System.out.println("\n----------Main Menu----------");
+            System.out.println("[1] Create a Vending Machine");
+            System.out.println("[2] Test a Vending Maachine");
+            System.out.println("[3] Exit"); 
+            System.out.print("Input: ");
             int nChoice = sc.nextInt();
             switch(nChoice) {
                 case 1:
-                    isValid = true;
+                    //isValid = true;
                     System.out.print("\033[H\033[2J");
                     System.out.println("Create Screen");
+                    String buff = sc.next();
+                    System.out.print("\033[H\033[2J");
                     break;
                 case 2:
-                    System.out.println("\nSorry! This feature is not yet available.");
-                    System.out.println("Please re-enter input:");
+                    System.out.println("Sorry! This feature is not yet available.\n");
                     break;
                 case 3:
-                    System.exit(0);
+                    System.out.println("\nThank you! Have a good day!");
+                    willLoop = true;
+                    break;
                 default:
                     System.out.println("Sorry, that is not option. Please re-enter input:");
                 }
-        }
+        } while (!willLoop);
         sc.close();
     }
 
