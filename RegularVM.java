8ce3921c645c649b4aaef2680bea1ea987a517ef
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-28 06:19:04 +0800

    added actual products

--- RegularVM.java
+++ RegularVM.java
@@ -4,12 +4,25 @@ public class RegularVM {
     private ArrayList<Item> slot;
 
     public RegularVM(){
-        
-        Item peper = new Item("boop", 432, 34,65);
-        Item yaur = new Item("yaur",23,213,323);
+        Item Pepperoni = new Item("Pepperoni", 69, 136, 10);
+        Item Ham = new Item("Ham", 69, 100, 10);
+        Item Sausage = new Item("Sausage", 69, 210, 10);
+        Item BBQ = new Item("BBQ Chicken", 69, 215, 10);
+        Item Mozarella = new Item("Mozarella", 69, 85, 10);
+        Item Parmesan = new Item("Parmesan", 69, 42, 10);
+        Item Jalapenos = new Item("Jalapenos", 69, 12.3, 10);
+        Item Mushroom = new Item("Mushroom", 69, 10.2, 10);
+        Item Olives = new Item("Olives", 69, 13.2, 10);
         slot = new ArrayList<>();
-        slot.add(peper);
-        slot.add(yaur);
+        slot.add(Pepperoni);
+        slot.add(Ham);
+        slot.add(Sausage);
+        slot.add(BBQ);
+        slot.add(Mozarella);
+        slot.add(Parmesan);
+        slot.add(Jalapenos);
+        slot.add(Mushroom);
+        slot.add(Olives);
     }
 
     public void displayItems(){
