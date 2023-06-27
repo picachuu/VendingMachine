b5c71bf011bc15b1f49f87760743896e052a667d
Niconicolo <niccolo_jimenez@dlsu.edu.ph>
2023-06-28 06:21:33 +0800

    Added Price

--- RegularVM.java
+++ RegularVM.java
@@ -4,15 +4,15 @@ public class RegularVM {
     private ArrayList<Item> slot;
 
     public RegularVM(){
-        Item Pepperoni = new Item("Pepperoni", 69, 136, 10);
-        Item Ham = new Item("Ham", 69, 100, 10);
-        Item Sausage = new Item("Sausage", 69, 210, 10);
-        Item BBQ = new Item("BBQ Chicken", 69, 215, 10);
-        Item Mozarella = new Item("Mozarella", 69, 85, 10);
-        Item Parmesan = new Item("Parmesan", 69, 42, 10);
-        Item Jalapenos = new Item("Jalapenos", 69, 12.3, 10);
-        Item Mushroom = new Item("Mushroom", 69, 10.2, 10);
-        Item Olives = new Item("Olives", 69, 13.2, 10);
+        Item Pepperoni = new Item("Pepperoni", 53, 136, 10);
+        Item Ham = new Item("Ham", 69, 40, 10);
+        Item Sausage = new Item("Sausage", 55, 210, 10);
+        Item BBQ = new Item("BBQ Chicken", 55, 215, 10);
+        Item Mozarella = new Item("Mozarella", 40, 85, 10);
+        Item Parmesan = new Item("Parmesan", 50, 42, 10);
+        Item Jalapenos = new Item("Jalapenos", 45, 12.3, 10);
+        Item Mushroom = new Item("Mushroom", 43, 10.2, 10);
+        Item Olives = new Item("Olives", 37, 13.2, 10);
         slot = new ArrayList<>();
         slot.add(Pepperoni);
         slot.add(Ham);
