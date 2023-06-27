8ce210fa2d0874854cd80cb5221077d2d0987316
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-28 06:03:23 +0800

    fix displayitems

--- RegularVM.java
+++ RegularVM.java
@@ -4,7 +4,12 @@ public class RegularVM {
     private ArrayList<Item> slot;
 
     public RegularVM(){
+        
+        Item peper = new Item("boop", 432, 34,65);
+        Item yaur = new Item("yaur",23,213,323);
         slot = new ArrayList<>();
+        slot.add(peper);
+        slot.add(yaur);
     }
 
     public void displayItems(){
@@ -12,14 +17,10 @@ public class RegularVM {
         System.out.println("-------------------------------------");
         System.out.printf("%10s%22s", "Name", "Price\n");
         System.out.println("-------------------------------------");
-        for (int i = 1; i < slot.size(); i++)
+        for (int i = 0; i < slot.size(); i++)
         {
-            String name = slot.get(i).getName();
-            //System.out.println(Item.getName());
-            System.out.printf("%d.) %-17s%10f₱", i, slot.get(i).getName(), slot.get(i).getPrice());
+            System.out.printf("%d.) %-17s%10.2fP\n", i+1, slot.get(i).getName(), slot.get(i).getPrice());
         }
-
-
     }
 
 
