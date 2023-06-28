2bc21a6de9d9358eea656a2326b96ef2b728949e
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-28 21:36:07 +0800

    fix test menu

--- VMFactory.java
+++ VMFactory.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class VMFactory {
     public static void main(String[] args) {
 
-        //Main Menu
         Scanner sc = new Scanner(System.in);
         boolean willLoop = false;
         boolean vmExists = false;
@@ -18,7 +17,7 @@ public class VMFactory {
             switch(nChoice) {
                 case 1:
                     System.out.print("\033[H\033[2J");
-                    boolean isValid = false;
+                    boolean willLoop2 = false;
 
                     do{
                         System.out.println("\n----------Create----------");
@@ -29,21 +28,23 @@ public class VMFactory {
                         int nCreateChoice = sc.nextInt();
                         switch(nCreateChoice) {
                             case 1:
-                                isValid = true;       
+                                willLoop2 = true;       
                                 System.out.print("\033[H\033[2J");                         
                                 System.out.println("\nRegular Vending Machine successfully created!\n");
                                 vmExists = true;
                                 break;
                             case 2:
+                                System.out.print("\033[H\033[2J");
                                 System.out.println("\nSorry! This feature is not yet available.");
                                 break;
                             case 3:
-                                isValid = true;
+                                willLoop2 = true;
+                                System.out.print("\033[H\033[2J");
                                 break;
                             default:
                                 System.out.println("\nSorry, that is not option. Please re-enter input.");
                         }
-                    }while (!isValid);
+                    }while (!willLoop2);
                     break;
                 case 2:
                     if (!vmExists)
@@ -55,53 +56,79 @@ public class VMFactory {
                     {
                         RegularVM vm = new RegularVM();
                         System.out.print("\033[H\033[2J");
-                        boolean isValid2 = false;
+                        boolean willLoop3 = false;
                        
                         do{                        
-                            System.out.printf("\nCurrent Balance: P%.2f\n", vm.getTotalIncome());
                             System.out.println("\n----------Test----------");
-                            System.out.println("[1] Insert Payment");
-                            System.out.println("[2] Order Item");
-                            System.out.println("[3] Receive Change");//when no change return money
-                            System.out.println("[4] Return to Main Menu");
+                            System.out.println("[1] Vending Features");
+                            System.out.println("[2] Maintenance Features");
+                            System.out.println("[3] Return to Main Menu");
                             System.out.print("Input: ");
                             int nTestChoice = sc.nextInt();
                             switch(nTestChoice) {
                                 case 1:
                                     System.out.print("\033[H\033[2J");
-                                    System.out.println("1");
-                                    String buff = sc.nextLine();
+                                    boolean willLoop4 = false;
+                                    do{                        
+                                        System.out.printf("Current Balance: P%.2f\n", vm.getTotalIncome());
+                                        System.out.println("\n-------Vending Features-------");
+                                        System.out.println("[1] Insert Payment");
+                                        System.out.println("[2] Order Item");
+                                        System.out.println("[3] Receive Change");//when no change return money
+                                        System.out.println("[4] Return to Main Menu");
+                                        System.out.print("Input: ");
+                                        int nVenChoice = sc.nextInt();
+                                        switch(nVenChoice) {
+                                            case 1:
+                                                System.out.print("\033[H\033[2J");
+                                                System.out.println("1");
+                                                String buff = sc.nextLine();
+                                                break;
+                                            case 2:
+                                                System.out.print("\033[H\033[2J");
+                                                vm.displayItems();
+                                                System.out.print("\nPlease enter item number: ");
+                                                
+                                                boolean willLoop5 = false;
+                                                do{
+                                                    int selOrder = sc.nextInt() - 1;
+                                                    if(selOrder >=0 && selOrder <= 8){
+                                                        willLoop5 = true;
+                                                        vm.orderItem(selOrder);
+                                                    }   
+                                                    else
+                                                        System.out.print("\nSorry, that is not an option. Please re-select: ");
+                                                }while(!willLoop5);
+                                                break;
+                                            case 3:
+                                                System.out.print("\033[H\033[2J");
+                                                System.out.println("3");
+                                                String buff3 = sc.nextLine();
+                                                break;
+                                            case 4:
+                                                willLoop4 = true;
+                                                System.out.print("\033[H\033[2J");
+                                                break;
+                                            default: 
+                                                System.out.println("\nSorry, that is not option. Please re-enter input:");
+                                        }
+                                        
+                                    }while(!willLoop4);
                                     break;
                                 case 2:
                                     System.out.print("\033[H\033[2J");
-                                    vm.displayItems();
-                                    System.out.print("\nPlease enter item number: ");
-                                    
-                                    boolean willLoop2 = false;
-                                    do{
-                                        int selOrder = sc.nextInt() - 1;
-                                        if(selOrder >=0 && selOrder <= 8){
-                                            willLoop2 = true;
-                                            vm.orderItem(selOrder);
-                                        }   
-                                        else
-                                            System.out.print("\nSorry, that is not an option. Please re-select: ");
-                                    }while(!willLoop2);
+                                    System.out.println("2");
+                                    String buff5 = sc.nextLine();
                                     break;
                                 case 3:
-                                    System.out.print("\033[H\033[2J");
-                                    System.out.println("3");
-                                    String buff3 = sc.nextLine();
-                                    break;
-                                case 4:
-                                    isValid2 = true;
+                                    willLoop3 = true;
                                     System.out.print("\033[H\033[2J");
                                     break;
                                 default: 
                                     System.out.println("\nSorry, that is not option. Please re-enter input:");
                             }
                             
-                        }while(!isValid2);
+                        }while(!willLoop3);
                     }
                     break;
                 case 3:
