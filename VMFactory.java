27a93f4492b2021038e6875575389749ec7c6970
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-06-30 06:08:31 +0800

    fix collectMoney

--- VMFactory.java
+++ VMFactory.java
@@ -141,7 +141,7 @@ public class VMFactory {
                                         System.out.println("\n-------Maintenance Features-------");
                                         System.out.println("[1] Restock Item");
                                         System.out.println("[2] Set Price");
-                                        System.out.println("[3] Collect Income");
+                                        System.out.println("[3] Collect Money");
                                         System.out.println("[4] Replenish Money");
                                         System.out.println("[5] View Denominations of Money");
                                         System.out.println("[6] Print Summary of Transactions");
@@ -189,8 +189,10 @@ public class VMFactory {
                                                 }while(newPrice%1!=0);
                                                 break;
                                             case 3:
+                                                System.out.print("\033[H\033[2J");
                                                 // System.out.println("3");
                                                 // String buff3 = sc.nextLine();
+                                                vm.collectMoney();
                                                 break;
                                             case 4:
                                                 boolean willLoop7 = true;
