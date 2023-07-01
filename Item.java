ce8d41235009973880ef8f0b14251be2f1ff7173
Niconicolo <niccolo_jimenez@dlsu.edu.ph>
2023-07-01 19:55:44 +0800

    Added Javadoc comments

--- Item.java
+++ Item.java
@@ -1,8 +1,19 @@
+/**
+ * Javadoc documentation by Niccolo G. Jimenez & Jannica S. Lim
+ * The class is a blueprint to create an object, the class "Item" creates an object for a vending machine item
+ * The class "Item" has 7 attributes: name, calories, price, stock;
+ */
 public class Item {
     private String name;
     private double calories, price;
     private int stock;
-
+    /**
+     * This method is the constructor that requests for the following
+     * @param name name of the item
+     * @param price price of the item
+     * @param calories calories of the item
+     * @param stock stock of the item
+     */
     public Item(String name, double price, double calories, int stock)
     {
         this.name = name;
@@ -10,19 +21,31 @@ public class Item {
         this.calories = calories;
         this.stock = stock;
     }
-
+    /**
+     * This method is the construtor that will request for the following
+     * @param name name of the item
+     * @param calories calories of the item
+     * @param stock stock of the item
+     */
     public Item(String name, double calories, int stock)
     {
         this.name = name;
         this.calories = calories;
         this.stock = stock;
     }
-
+    /**
+     * This method changes the price of the item given the following
+     * @param newPrice the new price of the item
+     */
     public void changePrice(double newPrice)
     {
         this.price = newPrice;
     }
-
+    /**
+     * This method adds stock of the current item
+     * @param addtoStock amount of stock to be added
+     * @return
+     */
     public boolean addStock(int addtoStock)
     {
         boolean check = true;
@@ -32,7 +55,11 @@ public class Item {
             check = false;
         return check;
     }
-
+    /**
+     * This item removes stock from the item
+     * @param removefromStock amount of stock to be removed
+     * @return
+     */
     public boolean removeStock(int removefromStock)
     {
          boolean check = true;
@@ -42,22 +69,34 @@ public class Item {
             check = false;
         return check;
     }
-
+    /**
+     * This method gets the name of the item
+     * @return String containing the name of the item
+     */
     public String getName()
     {
         return this.name;
     }
-
+    /**
+     * This method gets the calories of the item
+     * @return Double containing the calories of the item
+     */
     public double getCalories()
     {
         return this.calories;
     }
-
+    /**
+     * This method gets the price of the item
+     * @return Double containing the price of the item
+     */
     public double getPrice()
     {
         return this.price;
     }
-    
+    /**
+     * This method gets the stock of the item
+     * @return Integer containing the stock of the current item
+     */
     public int getStock()
     {
         return this.stock;
