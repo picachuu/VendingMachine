7b256cc87d2c030ce5c2946d759d8ca4f0671145
picachuu <jannica_allison_lim@dlsu.edu.ph>
2023-07-30 05:55:04 +0800

    remove javadoc

--- JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Item"},{"p":"<Unnamed>","l":"Money"},{"p":"<Unnamed>","l":"RegularVM"}];updateSearchResults();
